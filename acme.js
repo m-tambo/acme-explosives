4a4e53c4e4d2641eea1255fc607b97de768b737a
Michael Tambornino <michael.tambornino@gmail.com>
2017-01-04 17:38:17 -0600

    trying to target dropdown

--- acme.js
+++ acme.js
@@ -1,9 +1,8 @@
 // enable bootstrap dropdowns
 $('.dropdown-toggle').dropdown()
 
-$('#dropdownMenu1').change( function () {
+$('#dropdownMenu1').change(populateCards(e))
 
-})
 
 
 
@@ -11,13 +10,13 @@ $('#dropdownMenu1').change( function () {
 
 function populateCards () {
    for (let i = 0; i < 6; i++) {
+      if ()
       $('#grid').append(`
          <div class="container card col-sm-5 col-xs-5 col-md-4 col-lg-3">
             <h1>Firework ${i}</h1>
+            <h3>Type ${i}</h3>
             <p>Description ${i}</p>
          </div>
       `)
    }
 }
-
-populateCards()
---
+++ promises.js
@@ -0,0 +1,64 @@
+// define promises
+
+var promise1 = new Promise(function(resolve, reject){
+  var request1 = new XMLHttpRequest()
+  request1.addEventListener("load", function() {
+    var data = JSON.parse(request1.responseText).categories
+    resolve(data)
+  })
+  request1.open("GET", "json/categories.json")
+  request1.send()
+})
+
+var promise2 = new Promise(function(resolve, reject){
+  var request2 = new XMLHttpRequest()
+  request2.addEventListener("load", function() {
+    var data = JSON.parse(request2.responseText).types
+    resolve(data)
+  })
+  request2.open("GET", "json/types.json")
+  request2.send()
+})
+
+var promise3 = new Promise(function(resolve, reject){
+  var request3 = new XMLHttpRequest()
+  request3.addEventListener("load", function() {
+    var data = JSON.parse(request3.responseText).products
+    resolve(data)
+  })
+  request3.open("GET", "json/products.json")
+  request3.send()
+})
+
+
+
+
+
+ promise1
+  .then(
+  function(val){
+    categories = val
+    console.log("promise one resolved, ", categories)
+    return promise2
+  })
+  .then(
+    function(val) {
+      types = val
+      console.log("promise two resolved, ", types)
+      return promise3
+    })
+  .then(
+    function(val) {
+      products = val
+      console.log("promise three resolved, ", products)
+    })
+  .then(populateCards)
+
+
+// Promise.all([promise1, promise2, promise3])
+//   .then(function(values) {
+//     suspects = values[0]
+//     weapons = values[1]
+//     rooms = values[2]
+//     findGuess()
+//   })
