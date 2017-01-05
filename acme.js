fa80cede57dbb119445285fbdab7a1762539926c
Michael Tambornino <michael.tambornino@gmail.com>
2017-01-04 23:47:11 -0600

    looping adventures

--- acme.js
+++ acme.js
@@ -15,13 +15,20 @@ function resetCards () {
 
 function populateCards () {
    resetCards();
-   for (let i = 0; i < 6; i++) {
-      $('#grid').append(`
-         <div class="container card col-sm-5 col-xs-5 col-md-4 col-lg-3">
-            <h1>Firework ${i}</h1>
-            <h3>Type ${i}</h3>
-            <p>Description ${i}</p>
-         </div>
-      `)
+   let prods =[];
+   for (let j=0; j < products.length; j++) {
+      prods.push(products.childNode[j].type_id)
    }
+   console.dir(prods)
+      // for (let i = 0; i < 1; i++) {
+      //    if (prods[i].type_id === 0) {
+      //       $('#grid').append(`
+      //          <div class="container card col-sm-5 col-xs-5 col-md-4 col-lg-3">
+      //           <h1>Firework ${i}</h1>
+      //             <h3>Type ${i}</h3>
+      //             <p>Description ${i}</p>
+      //          </div>
+      //       `)
+      //    }
+      // }
 }
